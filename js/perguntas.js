99c5ed15f9571043811ddf615c2871e018df4b34
yanpaixao <107116103+yanpaixao@users.noreply.github.com>
2024-08-30 08:33:44 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,122 @@
+
+export const perguntas = [
+    {
+        enunciado: "como que a inteligencia artificial pode contribuir para nossa sociedade ?",
+        alternativas: [
+            { 
+                texto:"pode automatizar tarefas repetitivas e demoradas, liberando tempo para que as pessoas se encontrem em atividades mais recreativas e estrategicas.",
+               afirmacao:[ "pode analizar grandes quantiades de dados para entender preferencia e padroes de comportamento individual.",
+                 "A inteligencia artificial é um campo de estudo que se concentra no desenvolvimento de sistemas capazes de realizar tarefas que normalmente requeren inteligencia humana. "
+            ],
+        proxima: 1,
+           
+        },
+
+            {  
+              texto:"pode automatizar tarefas repetitivas e demoradas, liberando tempo para que as pessoas se encontrem em atividades mais recreativas e estrategicas.",
+               afirmacao:[ "pode analizar grandes quantiades de dados para entender preferencia e padroes de comportamento individual.",
+              "existem diferentes abordagems para a construcao de sistemas de ia incluindo aprendizado de máquinas, redes neurais e processamento de linguagem natural. "
+            ],
+            proxima: 2,
+        }
+        
+            
+        ]
+    
+
+    },
+
+    {
+        enunciado: "qual e os maleficios da inteligencia artificial?",
+        alternativas: [
+         {
+            texto:  "perda de enprego a medida que a ia se torna mais avançada é capaz de realizar tarefas que antes erao feitas por humanos",
+            afirmacao:[ "falta de enpatia, sistema de ia podem nao ser capazes de entender a empatia com as emoçoes humanas",
+         "A aprendizagem de máquinas é um subcampo da IA que envolve o desenvolvimento de algoritmos que podem aprender com dados sem serem explicitamente programado."
+            ],
+            proxima: 3,
+        
+        },
+        {
+            texto:  "perda de enprego a medida que a ia se torna mais avançada é capaz de realizar tarefas que antes erao feitas por humanos",
+            afirmacao:[  "falta de enpatia, sistema de ia podem nao ser capazes de entender a empatia com as emoçoes humanas",
+          "As redes neurais são um tipo de algotitmo de aprendizado de máquina que é modelado após a estrutura e a função do cérebro humano"
+            ],
+            proxima: 4,
+         }
+       
+    ]
+
+    },
+
+
+
+    {
+        enunciado: "como podemos usar a inteligencia artificial no nosso dia a dia?",
+        alternativas: [
+            {   
+            texto:  "asistente pessoal,navagaçao,emtreterimento.",
+            afirmacao:[  "comercio,saúde,finanças",
+           "As IA e a automação tornam processos como a tomada de decisões e a produção muito mais rápidos e eficazes, sendo muito úteis na otimização de nossas rotinas diárias.",
+
+            ],
+            proxima: 5,
+            },
+            { 
+              
+              texto:  "asistente pessoal,navagaçao,emtreterimento.",
+            afirmacao:[  "comercio,saúde,finanças",
+           "Entre seus pontos negativos, estão os elevados custos de produção e implementação e a possibilidade de ocasionarem o desemprego estrutural.",
+
+            ],
+            proxima: 6,
+            }
+        
+      ]
+    },
+
+
+    {
+        enunciado: "a inteligencia artificial é importante oje em dia?",
+        alternativas: [
+            { 
+                texto:   "sim, por conta da eficiencia para a procura de resposta imediatas. ",
+                afirmacao:[   "sim, pode analizar grandes quantidades de dados para atender preferencias e padroes de comportamento individual.",
+             "Podem apresentar falhas de controle que implicam risco àqueles que a controlam ou que fazem uso delas, como no caso dos carros autônomos.",
+
+                ],
+                proxima: 7,
+         },
+        { 
+         texto:   "sim, por conta da eficiencia para a procura de resposta imediatas. ",
+        afirmacao:[  "sim, pode analizar grandes quantidades de dados para atender preferencias e padroes de comportamento individual.",
+       "Os estudos sobre inteligência artificial ganharam força a partir da década de 1950, embora sempre tenham sido um tema muito debatido na ciência e pela sociedade.",
+
+
+        ],
+        proxima: 7,   
+    }
+     
+        ] 
+    },
+
+
+    {
+        enunciado: "quanto tempo pode durar o programa de inteligencia artificial?",
+        alternativas: [
+            {
+                texto:  "pode variar amplamente depemdendo do tamanho e complexidade do programa",
+                afirmacao:[  "o tempk de execuçao de um programa de ia tabem pode ser imfluenciado por conta de fatores como a velocidade de procesamento disponivel.",
+               "Riscos de segurança com relação à coleta ao armazenamento de dados pessoais, o que suscita intensos debates na sociedade atual.",
+
+                ],
+         },
+        {
+                texto:  "pode variar amplamente depemdendo do tamanho e complexidade do programa",
+                afirmacao:[   "o tempk de execuçao de um programa de ia tabem pode ser imfluenciado por conta de fatores como a velocidade de procesamento disponivel.",
+               "Têm avanços limitados mesmo com maior acúmulo de experiência.",
+             ],
+         },
+        ]
+    }
+];
